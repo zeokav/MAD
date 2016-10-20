67b54b1f5ed68718c189db6469dbcfcd9afb264a
zeokav <sakettsingh@outlook.com>
2016-10-20 16:33:42 +0530

    Added service

--- app/src/main/java/com/example/saket/musicplayer/MusicService.java
+++ app/src/main/java/com/example/saket/musicplayer/MusicService.java
@@ -2,17 +2,70 @@ package com.example.saket.musicplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Binder;
 import android.os.IBinder;
+import android.media.MediaPlayer;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
 
 /**
  * Created by saket on 10/20/2016.
  */
-public class MusicService extends Service{
+public class MusicService extends Service
+        implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener{
+
+
+    private MediaPlayer mPlayer;
+    private ArrayList<Song> songs;
+    private int position;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        position = 0;
+        mPlayer = new MediaPlayer();
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer() {
+        mPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mPlayer.setOnErrorListener(this);
+        mPlayer.setOnPreparedListener(this);
+        mPlayer.setOnCompletionListener(this);
+    }
+
+    public void setList(ArrayList<Song> songlist) {
+        songs = songlist;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+
+    }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
     }
 }
